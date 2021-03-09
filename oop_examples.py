0357983171f39fae237cc4ac19d41c2433e68540
David <david.krtolica8@gmail.com>
2021-03-09 13:43:45 +0100

    Started working on Python OOP

---
+++ oop_examples.py
@@ -0,0 +1,32 @@
+class Person:
+    spicies = 'Mammal' ## CLASS VARIABLE, SHARED BY ALL OBJECTS CREATED (STATIC) 
+
+    #def __init__(self, name): ## UNIQUE INSTANCE VARIABLE WHICH IS DYNAMIC
+        #self.name = name
+
+    def __init__(self, *args): 
+        if len(args) == 1:
+            self.name = args[0]
+        elif len(args) == 2:
+            self.name = args[0]
+            self.age = args[1]
+        else:
+            self.name = 'No No No'
+
+    #def __init__(self, name=None, age=None):
+        #self.name = name
+        #self.age = age
+
+
+
+class Instructor:
+    def __init__(self, course):
+        self.course = course
+
+    
+
+class Student(Person, Instructor):
+    def __init__(self, name, course):
+        Person.__init__(self, name)
+        Instructor.__init__(self, course)
+        #super().__init__(name)
