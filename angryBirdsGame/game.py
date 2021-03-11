e4a89d3c29aa1c3c13cb72d7269937d0de7f58c4
David <david.krtolica8@gmail.com>
2021-03-11 22:12:15 +0100

    Finished AngryBirds game! Working nicely

--- angryBirdsGame/game.py
+++ angryBirdsGame/game.py
@@ -1,32 +1,82 @@
+import random
+
 class Bird:
     def __init__(self, x, y, direction, icon):
         self.x = x
         self.y = y
         self.direction = direction
         self.icon = icon
-    def move(userInput):
-        if (userInput == 'r'):
-            direction += 1 
-        elif (userInput == 'l'):
-            direction -= 1
-        if (userInput == 'f' and direction == 0 or direction == 2):
-            y += 1
-        elif (userInput == 'f' and direction == 1 or direction == 3):
-            x += 1
+
 
 class Pig:
     def __init__(self, x, y, icon):
         self.x = x
         self.y = y
-        self.icon = 'P'
+        self.icon = icon
 
 
-class Board:
+class Board():
     def __init__(self):
-        self.Bird = Bird(2, 4, 1, 'B')
-        self.Pig = Pig(6, 5, 'P')
-    def createBoard(self):
-        x = 10
-        y = 10
-        for i in range(y):
-            print (' *' * x + ' ')
+        self.Bird = Bird(random.randint(1,10), random.randint(1,10), 1, 'B')
+        self.Pig = Pig(random.randint(1,10), random.randint(1,10), 'P')
+
+    def printBoard(self):
+        board = [["*" for a in range(10)] for b in range(10)]
+        board[self.Bird.y-1][self.Bird.x-1] = self.Bird.icon
+        board[self.Pig.y-1][self.Pig.x-1] = self.Pig.icon
+        for i in board:
+            print(" ".join(i))        
+
+
+class Workspace():
+    def __init__(self):
+        self.instructions = ("What steps do you want to perform? \n"
+        "Options: move forward(f), turn left(l), turn right(r) \n"
+        "Type \"q\" when finished")
+
+    def printInstr(self):
+        print(self.instructions)
+
+
+class Game():
+    def __init__(self):
+        self.Board = Board()
+        self.Workspace = Workspace()
+
+    def run(self):
+        self.Board.printBoard()
+        self.Workspace.printInstr()
+        userInput = ""
+        while (userInput != 'q'):
+            userInput = input("Move: ")
+            if (userInput == 'r'):
+                self.Board.Bird.direction += 1
+                self.Board.printBoard()
+            elif (userInput == 'l'):
+                self.Board.Bird.direction -= 1
+                self.Board.printBoard()
+            if (userInput == 'f'):
+                if (self.Board.Bird.direction == 0):
+                        self.Board.Bird.y -= 1
+                        self.Board.printBoard()
+                elif (self.Board.Bird.direction == 2):
+                        self.Board.Bird.y += 1
+                        self.Board.printBoard()
+                elif (self.Board.Bird.direction == 1):
+                    self.Board.Bird.x += 1
+                    self.Board.printBoard()
+                elif (self.Board.Bird.direction == 3):
+                    self.Board.Bird.x -= 1
+                    self.Board.printBoard()
+        if (self.Board.Bird.x == self.Board.Pig.x and self.Board.Bird.y == self.Board.Pig.y):
+            print("Ufff the pig is dead!")
+            print(f'Birds position: [{self.Board.Bird.x}, {self.Board.Bird.y}]')
+            print(f'Pigs position: [{self.Board.Pig.x}, {self.Board.Pig.y}]')
+        else:
+            print("You missed :(")
+            print(f'Birds position: [{self.Board.Bird.x}, {self.Board.Bird.y}]')
+            print(f'Pigs position: [{self.Board.Pig.x}, {self.Board.Pig.y}]')
+
+#RUN THE GAME
+g = Game()
+g.run()
